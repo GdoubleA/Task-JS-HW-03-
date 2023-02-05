eef406d1752a420b6103fa596c14abe7285651f2
GdoubleA <supertemac@gmail.com>
2023-02-04 23:54:15 -0300

    trafficL

--- task3/script.js
+++ task3/script.js
@@ -1,3 +1,4 @@
+// Automatic TrafficLight
 const circles = document.querySelectorAll('.circle');
 console.log(circles);
 let activeLight = 0;
@@ -20,6 +21,56 @@ function changeLight() {
 }
 
 
+// Click TrafficLight
+
+const trafficLightEl = document.querySelector('#trafficLight');
+const trafficLightE2 = document.querySelector('#trafficLight2');
+const trafficLightE3 = document.querySelector('#trafficLight3');
+
+
+function makeGreen() {
+    trafficLightEl.style.background = ('green');
+    trafficLightEl.removeEventListener('click', makeGreen);
+    trafficLightEl.addEventListener('click', makeYellow);
+    trafficLightEl.addEventListener('click', () => {
+        trafficLightEl.style.background = ('');
+        trafficLightEl.addEventListener('click', () => {
+            trafficLightE3.style.background = ('red');
+            trafficLightE2.style.background = ('')
+        })   
+    })
+}
+
+function makeYellow() {
+    trafficLightE2.style.background = ('yellow');
+    trafficLightE2.removeEventListener('click', makeYellow);
+    trafficLightE2.addEventListener('click', makeRed);
+    trafficLightE2.addEventListener('click', () => {
+        trafficLightE2.style.background = ('');
+        trafficLightE2.addEventListener('click', () => {
+            trafficLightEl.style.background = ('green');
+            trafficLightE3.style.background = ('');
+            
+        })
+    })
+}
+
+function makeRed() {
+    trafficLightE3.style.background = ('red');
+    trafficLightE3.removeEventListener('click', makeRed);
+    trafficLightE3.addEventListener('click', makeGreen);
+    trafficLightE3.addEventListener('click', () => {
+        trafficLightE3.style.background = ('');
+        trafficLightE3.addEventListener('click', () => {
+            trafficLightE2.style.background = ('yellow');
+            trafficLightEl.style.background = ('');
+        })
+    })
+}
+
+    trafficLightEl.addEventListener('click', makeGreen);
+    trafficLightE2.addEventListener('click', makeYellow);
+    trafficLightE3.addEventListener('click', makeRed);
 
 
 
@@ -27,50 +78,30 @@ function changeLight() {
 
 
 
+//  One TrafficLight
 
+const trafficLightE1 = document.querySelector('#trafficLightE1');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function makeGreen() {
-//     trafficLightEl.style.background = ('green');
-//     trafficLightEl.removeEventListener('click', makeGreen);
-//     trafficLightEl.addEventListener('click', makeYellow);
-
-//     trafficLightEl.addEventListener('click', makeRed);
-// }
+function Green() {
+    trafficLightE1.style.background = ('green');
+    trafficLightE1.removeEventListener('click', Green);
+    trafficLightE1.addEventListener('click', Yellow);
+}
     
+function Yellow() {
+    trafficLightE1.style.background = ('yellow');
+    trafficLightE1.removeEventListener('click', Yellow);
+    trafficLightE1.addEventListener('click', Red);
+}
 
+function Red() {
+    trafficLightE1.style.background = ('red');
+    trafficLightE1.removeEventListener('click', Red);
+    trafficLightE1.addEventListener('click', Green);
+}
 
+trafficLightE1.addEventListener('click', Green);
 
-// function makeYellow() {
-//     trafficLightE2.style.background = ('yellow');
-//     trafficLightE2.removeEventListener('click', makeYellow);
-//     trafficLightE2.addEventListener('click', makeRed);
-// }
-
-
-// function makeRed() {
-//     trafficLightE3.style.background = ('red');
-//     trafficLightE3.removeEventListener('click', makeRed);
-//     trafficLightE3.addEventListener('click', makeGreen);
-// }
-
-// trafficLightEl.addEventListener('click', makeGreen);
-// trafficLightE2.addEventListener('click', makeYellow);
-// trafficLightE3.addEventListener('click', makeRed);
 
 
 
